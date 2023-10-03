fa3ec91f407a729f055180b6e27bd53686f14b91
abdelhamied <abdelhamied403@gmail.com>
2023-10-03 23:30:24 +0300

    chat worked

--- src/containers/Chat.tsx
+++ src/containers/Chat.tsx
@@ -34,53 +34,50 @@ const Chat = () => {
     },
   ];
   return (
-    <div className="h-full">
-      <div className="flex flex-col justify-between gap-3">
-        <div className="flex items-center justify-between p-6 bg-primary-100 rounded-xl">
-          <div className="flex gap-3 items-center">
-            <img
-              src="https://placehold.co/400x400/fee/31343C"
-              alt="almotarjim"
-              className="w-14 h-14 rounded-full"
-            />
-            <h3>name</h3>
-          </div>
-          <div className="flex gap-3">
-            <Button className="">Create Request</Button>
-            <Button variant={"danger"} className="flex gap-2 items-center">
-              End Chat <HiLogout />
-            </Button>
-          </div>
+    <>
+      <div className="flex flex-wrap items-center justify-between gap-4 p-6 bg-primary-100 rounded-xl">
+        <div className="flex gap-3 items-center">
+          <img
+            src="https://placehold.co/400x400/fee/31343C"
+            alt="almotarjim"
+            className="w-14 h-14 rounded-full"
+          />
+          <h3>name</h3>
         </div>
-        <div className="flex flex-col gap-4 p-8 bg-white rounded-2xl overflow-y-auto space-y-8">
-          {messages.map((msg) => (
-            <Message {...msg} />
-          ))}
-        </div>
-
-        <div className="flex items-center p-6 bg-primary-100 rounded-xl gap-3">
-          <div className="flex gap-3 items-center flex-1">
-            <img
-              src="https://placehold.co/400x400/fee/31343C"
-              alt="almotarjim"
-              className="w-14 h-14 rounded-full"
-            />
-            <Input placeholder="Type a message...." className=""></Input>
-          </div>
-          <div className="flex gap-3 justify-end items-end ">
-            <Button variant={"outline"} size={"icon"}>
-              <HiMicrophone />
-            </Button>
-            <Button variant={"outline"} size={"icon"}>
-              <HiPaperClip />
-            </Button>
-            <Button size={"icon"}>
-              <IoSend />
-            </Button>
-          </div>
+        <div className="flex gap-3">
+          <Button className="">Create Request</Button>
+          <Button variant={"danger"} className="flex gap-2 items-center">
+            End Chat <HiLogout />
+          </Button>
         </div>
       </div>
-    </div>
+      <div className="flex-1 h-full flex flex-col gap-4 p-8 bg-white rounded-2xl overflow-y-auto space-y-8">
+        {messages.map((msg) => (
+          <Message {...msg} />
+        ))}
+      </div>
+      <div className="flex items-center p-6 bg-primary-100 rounded-xl gap-3">
+        <div className="flex gap-3 items-center flex-1">
+          <img
+            src="https://placehold.co/400x400/fee/31343C"
+            alt="almotarjim"
+            className="w-14 h-14 rounded-full"
+          />
+          <Input placeholder="Type a message...." className=""></Input>
+        </div>
+        <div className="flex gap-3 justify-end items-end ">
+          <Button variant={"outline"} size={"icon"}>
+            <HiMicrophone />
+          </Button>
+          <Button variant={"outline"} size={"icon"}>
+            <HiPaperClip />
+          </Button>
+          <Button size={"icon"}>
+            <IoSend />
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
