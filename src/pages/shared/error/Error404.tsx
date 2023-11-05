31636c3686dbab0efd688a8e0fd15a84d4f5ff21
ahmedrabie <a.rabie.eissa@gmail.com>
2023-11-05 22:13:24 +0200

    error loading page

--- src/pages/shared/error/Error404.tsx
+++ src/pages/shared/error/Error404.tsx
@@ -7,13 +7,14 @@ const Error404 = () => {
     <>
       <div className="flex items-center justify-center h-screen w-screen bg-primary-600">
         <div className="grid md:grid-cols-2 gap-10 items-center ">
-          <div className="flex flex-col text-white text-4xl gap-10 text-center">
+          <div className="flex flex-col text-white text-2xl md:text-4xl gap-10 text-center">
+            <h1>! OOPS</h1>
             <h1>404</h1>
             <Link to="/">
               <Button variant={"error"}>Go To Home Page</Button>
             </Link>
           </div>
-          <div className="order-first">
+          <div className="order-first md:order-last">
             <img src={error} alt="404 not found" />
           </div>
         </div>
